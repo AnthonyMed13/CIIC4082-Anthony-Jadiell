772e01285cf970aa62dac80b46f9c5d38335b825
AnthonyMed13 <anthony.medina6@upr.edu>
2024-03-31 04:21:25 -0400

    Added controller  support to finish task 3

--- src/reset.asm
+++ src/reset.asm
@@ -40,42 +40,12 @@ vblankwait2:
   STA player1_x
   LDA #$40
   STA player1_y
-  LDA #$70
-  STA player2_x
-  LDA #$50
-  STA player2_y
-  LDA #$70
-  STA player3_x
-  LDA #$60
-  STA player3_y
-  LDA #$70
-  STA player4_x
-  LDA #$70
-  STA player4_y
   LDA #$00
   STA player1_dir
   LDA #$01
-  STA player2_dir
-  LDA #$03
-  STA player3_dir
-  LDA #$02
-  STA player4_dir
-  LDA #$01
   STA player1_ws
-  LDA #$01
-  STA player2_ws
-  LDA #$01
-  STA player3_ws
-  LDA #$01
-  STA player4_ws
   LDA #$00
   STA player1_cs
-  LDA #$00
-  STA player2_cs
-  LDA #$00
-  STA player3_cs
-  LDA #$00
-  STA player4_cs
   LDA #$01
   STA player1_ult
   LDA #$02
@@ -84,35 +54,9 @@ vblankwait2:
   STA player1_llt
   LDA #$12
   STA player1_lrt
-  LDA #$21
-  STA player2_ult
-  LDA #$22
-  STA player2_urt
-  LDA #$31
-  STA player2_llt
-  LDA #$32
-  STA player2_lrt
-  LDA #$27
-  STA player3_ult
-  LDA #$28
-  STA player3_urt
-  LDA #$37
-  STA player3_llt
-  LDA #$38
-  STA player3_lrt
-  LDA #$07
-  STA player4_ult
-  LDA #$08
-  STA player4_urt
-  LDA #$17
-  STA player4_llt
-  LDA #$18
-  STA player4_lrt
+ 
 
   LDA #$00
   STA frame_counter1
-  STA frame_counter2
-  STA frame_counter3
-  STA frame_counter4
   JMP main
 .endproc
